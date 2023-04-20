5115eafbc360bf87d74d0be5b6eca625490bfbce
melcd238 <melcd238@gmail.com>
2023-04-20 17:38:39 +0200

    refactoring CustomerCsvFileWriter , created function validateFileName and validateCustomers

--- customer-csv-file-writer.ts
+++ customer-csv-file-writer.ts
@@ -6,15 +6,24 @@ export class CustomerCsvFileWriter {
    constructor(private fileSystemWriter: FileSystemWriter) { }
 
     writeCustomers(fileName: string, customers: Customer[]) {
-        if (customers.length === 0) {
-            throw new Error("No customer provided");
-        }
-        if(!fileName){
-            throw new Error("No file name provided");
-        }
+        this.validateFileName(fileName);
+        this.validateCustomers(customers);
+        
         for (let index = 0; index < customers.length; index++) {
             const customer = customers[index];
             this.fileSystemWriter.writeLine(fileName,`${customer.name},${customer.contactNumber}`);
+        }
+    }
+
+    private validateCustomers(customers: Customer[]): void {
+        if (customers.length === 0) {
+            throw new Error("No customer provided");
+        }
+    }
+
+    private validateFileName(fileName: string):void {
+        if(!fileName){
+            throw new Error("No file name provided");
         }
     }
 }
